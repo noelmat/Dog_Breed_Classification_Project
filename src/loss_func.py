209102844c047cf7a782efe07509389cb51e9827
Noel Mathew <noel.mathew007@gmail.com>
2020-10-02 22:04:37 +0000

    refact: Refactored modelutils for flake8

--- src/loss_func.py
+++ src/loss_func.py
@@ -6,13 +6,10 @@ class CustomLoss(nn.Module):
     Custom loss function, a combination of dog and human classification losses with
     breed classification loss. The loss function uses two BCEWithLogitsLoss, one for
     human and dog each, and a CrossEntropyLoss for the dog breed classification.
+            loss = dog_loss + human_loss + breed_loss
 
-    loss = dog_loss + human_loss + breed_loss
-
-    Args: 
+    Args:
         dog_human_labeller : Labeller used for human vs dog classification.
-
-
     """
     def __init__(self, dog_human_labeller):
         """
@@ -29,16 +26,16 @@ class CustomLoss(nn.Module):
         self.dog_human_labeller = dog_human_labeller
         self.human_idx = self.dog_human_labeller.label_dict['human']
         self.dog_idx = self.dog_human_labeller.label_dict['dog']
-      
+
     def forward(self, outputs, t1, t2):
         """
         Calculates human_loss, dog_loss and breed_loss and return a combination
         of the three.
 
-        Args: 
+        Args:
             outputs: activations from the model
             t1: OneHot Encoded Targets for human vs dog classification.
-            t2: Targets for dog breed classification. 
+            t2: Targets for dog breed classification.
         """
         h_loss = self.human_loss(outputs[:, [self.human_idx]],
                                  t1[:, [self.human_idx]])
@@ -50,4 +47,3 @@ class CustomLoss(nn.Module):
         b_loss = self.breed_loss(outputs[mask, 2:], t2[mask])
         loss = h_loss + d_loss + b_loss
         return loss
-        
--- src/modelutils.py
+++ src/modelutils.py
@@ -1,49 +1,56 @@
+import torch
 import torch.nn as nn
-import torch.nn.functional as F
+
 
 def conv_layer(f_in, f_out, ks, s, p):
-    return nn.Sequential(nn.Conv2d(f_in, f_out, kernel_size=ks, stride=s, padding=p,bias=False),
-          nn.BatchNorm2d(f_out),
-          nn.ReLU())
+    return nn.Sequential(
+        nn.Conv2d(f_in, f_out, kernel_size=ks, stride=s, padding=p,bias=False),
+        nn.BatchNorm2d(f_out),
+        nn.ReLU())
+
 
 class ResBlock(nn.Module):
     def __init__(self, nf):
         super().__init__()
         self.nf = nf
-        self.conv1 = conv_layer(nf,nf,3,1,1)
-        self.conv2 = conv_layer(nf,nf,3,1,1)``
+        self.conv1 = conv_layer(nf, nf, 3, 1, 1)
+        self.conv2 = conv_layer(nf, nf, 3, 1, 1)
 
     def forward(self, X):
         return X + self.conv2(self.conv1(X))
+
 
 class DenseBlock(nn.Module):
     def __init__(self, ni, nf):
         super().__init__()
         self.ni, self.nf = ni, nf
-        self.conv1 = conv_layer(ni, nf,3,1,1)
-        self.conv2 = conv_layer(nf, nf,3,1,1)
-  
+        self.conv1 = conv_layer(ni, nf, 3, 1, 1)
+        self.conv2 = conv_layer(nf, nf, 3, 1, 1)
+
     def forward(self, X):
-        return torch.cat([X,self.conv2(self.conv1(X))],dim=1)
+        return torch.cat([X, self.conv2(self.conv1(X))], dim=1)
+
 
 class AdaptivePooling(nn.Module):
     def __init__(self, ni):
         super().__init__()
         self.max_pool = nn.AdaptiveMaxPool2d(ni)
         self.avg_pool = nn.AdaptiveAvgPool2d(ni)
-    
+
     def forward(self, x):
         max_pool = self.max_pool(x)
         avg_pool = self.avg_pool(x)
         return max_pool + avg_pool
 
+
 class Lambda(nn.Module):
     def __init__(self, func):
         super().__init__()
         self.func = func
-    
+
     def forward(self, x):
         return self.func(x)
+
 
 def flatten(x):
     return x.view(x.shape[0], -1)
