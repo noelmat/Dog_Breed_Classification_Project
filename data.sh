e6ac3f5d16b6d421ed262e57626c79d205ee5d21
Noel Mathew <noel.mathew007@gmail.com>
2020-10-04 21:09:19 +0000

    fix: added support for curl if wget not available

--- data.sh
+++ data.sh
@@ -1,7 +1,11 @@
 mkdir input
-wget https://s3-us-west-1.amazonaws.com/udacity-aind/dog-project/dogImages.zip -P input/
-wget https://s3-us-west-1.amazonaws.com/udacity-aind/dog-project/lfw.zip -P input/
-
+if which wget
+then 
+    wget https://s3-us-west-1.amazonaws.com/udacity-aind/dog-project/dogImages.zip -P input/
+    wget https://s3-us-west-1.amazonaws.com/udacity-aind/dog-project/lfw.zip -P input/
+else
+    cd input && { curl -O https://s3-us-west-1.amazonaws.com/udacity-aind/dog-project/dogImages.zip ; cd -; }
+    cd input && { curl -O https://s3-us-west-1.amazonaws.com/udacity-aind/dog-project/lfw.zip ; cd -; }
 cd input
 unzip dogImages.zip
 unzip lfw.zip
